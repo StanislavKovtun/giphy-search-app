2191512c990a6b78719803a11927ecb1362ea433
Stanislav Kovtun <sstanisslaw@gmail.com>
2024-04-22 17:38:39 +0300

    autoPrerender

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  //TODO: is it correct way to do it?
+  //devIndicators: {
+  //  autoPrerender: false,
+  //},
+  //It works in this file format next.config.js:
+  //module.exports = {
+  //  devIndicators: {
+  //    autoPrerender: false,
+  //  },
+  //}
 };
 
 export default nextConfig;
