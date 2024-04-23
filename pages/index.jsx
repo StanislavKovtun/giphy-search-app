f76c7fe95141c7e5276fb0be5c3b8a449898d70b
Stanislav Kovtun <sstanisslaw@gmail.com>
2024-04-23 21:26:31 +0300

    Render giphy data

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ const GIPHY_KEY = "iydOb0v8bvpqj2cHU01dkRKjZMihahUn";
 
 export default function Home(initialData) {
 	useEffect(() => {
-		console.log(initialData);
+		//console.log("### initialData:", initialData);
 	})
 
 	return (
@@ -17,12 +17,30 @@ export default function Home(initialData) {
 			</Head>
 
 			<h1>Giphy Search App</h1>
+
+			<div class="giphy-search-results-grid">
+				{initialData.catGiphys.data.map((each, index) => {
+					return (
+						<div key={index}>
+							<h3>{each.title}</h3>
+							<img src={each.images.original.url} alt={each.title} />
+						</div>
+					)
+				})}
+			</div>
+
 		</div>
 	)
 }
 
 export async function getStaticProps() {
-	let catGiphys = await fetch(`https://api.giphy.com/v1/gifs/search?q=cats&api_key=${GIPHY_KEY}&limit=10`)
-	catGiphys = await catGiphys.json()
-	return { props: { catGiphys: catGiphys } }
+	let catGiphys = await fetch(`https://api.giphy.com/v1/gifs/search?q=cats&api_key=${GIPHY_KEY}&limit=10`);
+	console.log("### catGiphys:", catGiphys);
+	catGiphys = await catGiphys.json();
+	return {
+		props: {
+			catGiphys: catGiphys,
+			testProp: 'test'
+		}
+	}
 }
