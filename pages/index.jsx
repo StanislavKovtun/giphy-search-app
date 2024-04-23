8642447760fe60c464dde46c1ca45f95eeb625c6
Stanislav Kovtun <sstanisslaw@gmail.com>
2024-04-23 17:30:47 +0300

    Added about page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,15 @@
 import Head from 'next/head'
 
 export default function Home() {
-  return (
-    <div className='container'>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="/styles.css" />
-      </Head>
+	return (
+		<div className='container'>
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="stylesheet" href="/styles.css" />
+			</Head>
 
-      <h1>Giphy Search App</h1>
-    </div>
-  )
+			<h1>Giphy Search App</h1>
+		</div>
+	)
 }
