fed27ddeff701a41916de7ba91a43cf08600f9ca
Stanislav Kovtun <sstanisslaw@gmail.com>
2024-04-25 20:04:15 +0300

    Used getServerSideProps

--- pages/index.jsx
+++ pages/index.jsx
@@ -64,7 +64,35 @@ export default function Home(initialData) {
 	)
 }
 
-export async function getStaticProps() {
+// 1. Statically generated page
+
+//When HTML gets generated#
+//Lastly, if you remember from a previous lesson, statically generated pages are created at build time.
+//That means that when someone requests that page, the server has the HTML in hand and serves it to the user right away.
+// When you are server-side rendering a page, the server creates the HTML for every request.
+
+//export async function getStaticProps() {
+//	let catGiphys = await fetch(`https://api.giphy.com/v1/gifs/search?q=${INIT_SEARCH_TERM}&api_key=${GIPHY_KEY}&limit=10`);
+//	catGiphys = await catGiphys.json();
+//	return {
+//		props: {
+//			catGiphys: catGiphys,
+//			testProp: 'test'
+//		}
+//	}
+//}
+
+// 2. Dynamically generated page (SSR)
+
+//When should you server-side render a page?#
+//A good way to judge if server-side rendering a page is the right page type for you is to ask yourself a few questions.
+
+//Will the data change often?
+//Is the most recent data important?
+//Will the page use route parameters in order to determine which data it will retrieve?
+//If you answered yes to any of those questions, you will need to server-side render the page.
+
+export async function getServerSideProps() {
 	let catGiphys = await fetch(`https://api.giphy.com/v1/gifs/search?q=${INIT_SEARCH_TERM}&api_key=${GIPHY_KEY}&limit=10`);
 	catGiphys = await catGiphys.json();
 	return {
